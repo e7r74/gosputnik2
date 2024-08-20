ac778258ddceb4ac1c96a47a8d3712adbc8d1c6c
Yerkhan <127944702+e7r74@users.noreply.github.com>
2024-08-20 13:53:36 +0500

    Update PclubApplication.java

--- src/main/java/com/pclup/Pclub/PclubApplication.java
+++ src/main/java/com/pclup/Pclub/PclubApplication.java
@@ -13,17 +13,17 @@ public class PclubApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PclubApplication.class, args);
 	}
-//	@Configuration
-//	public static class Myconfiguration{
-//		@Bean
-//		public WebMvcConfigurer corsConfigurer(){
-//			return new WebMvcConfigurer() {
-//				@Override
-//				public void addCorsMappings(CorsRegistry registry) {
-//					registry.addMapping("/**")
-//							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-//				}
-//			};
-//		}
-//	}
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
